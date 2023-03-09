ab5e40d92561ff9ed252bcf3b23f65af69727131
Pamela Cruz <117556345+pmlgcz@users.noreply.github.com>
2023-03-09 11:01:44 -0300

    Update ativgetapi.js

--- ativgetapi.js
+++ ativgetapi.js
@@ -2,11 +2,11 @@ import express from "express";
 const app = express()
 
 app.get('/entidadetarefa', function (req, res) {
-  res.send("faculdade", "preparar marmita", "judô")
+  res.send("faculdade, preparar marmita, judô")
 })
 
 app.get('/entidadepessoas', function (req, res) {
-    res.send("pessoa1")
+    res.send("pessoa1, pessoa2, pessoa3")
   })
 
 app.listen(3000)
